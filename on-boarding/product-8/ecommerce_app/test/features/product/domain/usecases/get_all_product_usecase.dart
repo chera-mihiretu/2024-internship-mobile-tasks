2689c13dd2a9659f02b89b7701382300b09896f0
chera-mihiretu <cheramihiretucm@gmail.com>
2024-08-05 08:52:38 -0400

    fixing errors and also adding test for single product usecase

--- on-boarding/product-8/ecommerce_app/test/features/product/domain/usecases/get_all_product_usecase.dart
+++ on-boarding/product-8/ecommerce_app/test/features/product/domain/usecases/get_all_product_usecase.dart
@@ -1,7 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:ecommerce_app/features/product/domain/entities/product.dart';
 import 'package:ecommerce_app/features/product/domain/usecases/get_all_products_usecase.dart';
-import 'package:ecommerce_app/features/product/domain/usecases/get_product_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -9,30 +8,31 @@ import '../../../../test_helper/test_helper_generation.mocks.dart';
 
 void main() {
   late MockProductRepository mockProductRepository;
-  late GetProductUseCase getProductUseCase;
+  late GetAllProductUseCase getAllProductUseCase;
 
   setUp(() {
     mockProductRepository = MockProductRepository();
-    getProductUseCase = GetProductUseCase(mockProductRepository);
+    getAllProductUseCase = GetAllProductUseCase(mockProductRepository);
   });
-  int id = 0;
-  const  testingProducts =
+
+  const List<ProductEntity> testingProducts = [
     ProductEntity(
       id: 0,
       name: 'Derby Leather Sheos',
       description: 'men\'s shoes',
       price: 150.0,
-      imageUrl: '',);
+      imageUrl: '',),
+  ];
 
   test('Testing the data flow inside the Repositrory of product list return', () async {
     /// Rearranging the functionality
     when(
-        mockProductRepository.getProduct(id)
+        mockProductRepository.getAllProducts()
     ).thenAnswer((_) async => const Right(testingProducts));
 
     /// action
 
-    final result = await getProductUseCase.execute(id);
+    final result = await getAllProductUseCase.execute();
 
     /// assertion
     ///
