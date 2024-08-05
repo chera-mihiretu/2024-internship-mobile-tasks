a242b82b80cf25eb2899f7c521b89d8ac6eab682
chera-mihiretu <cheramihiretucm@gmail.com>
2024-08-05 08:32:21 -0400

    Delete Product test success

--- on-boarding/product-8/ecommerce_app/test/features/product/domain/usecases/delete_product_usecase_test.dart
+++ on-boarding/product-8/ecommerce_app/test/features/product/domain/usecases/delete_product_usecase_test.dart
@@ -1,1 +1,35 @@
-void main(){}
+import 'package:dartz/dartz.dart';
+import 'package:ecommerce_app/features/product/domain/usecases/delete_product_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../test_helper/test_helper_generation.mocks.dart';
+
+void main(){
+  late MockProductRepository mockProductRepository;
+  late DeleteProductUseCase deleteProductUseCase;
+
+  setUp((){
+    mockProductRepository = MockProductRepository();
+    deleteProductUseCase = DeleteProductUseCase(mockProductRepository);
+  });
+
+  int id = 8;
+
+  test('Testing the data flow inside the Repositrory', () async {
+    /// Rearranging the functionality
+    when(
+      mockProductRepository.deleteProduct(id)
+    ).thenAnswer((_)async =>  const Right(1));
+
+    /// action
+
+    final result = await deleteProductUseCase.execute(id);
+
+    /// assertion
+    ///
+    expect(result, const Right(1));
+
+
+  });
+}
--- on-boarding/product-8/ecommerce_app/test/test_helper/test_helper_generation.dart
+++ on-boarding/product-8/ecommerce_app/test/test_helper/test_helper_generation.dart
@@ -1,1 +1,13 @@
-void main(){}
+import 'package:ecommerce_app/features/product/domain/repositories/product_repository.dart';
+import 'package:mockito/annotations.dart';
+
+
+@GenerateMocks(
+  [
+    ProductRepository
+  ]
+)
+
+void main(){
+
+}
