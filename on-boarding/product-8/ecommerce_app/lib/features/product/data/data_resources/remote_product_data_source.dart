370ff488f8e1762e36238d73c4ce3c0d3c120cb0
chera-mihiretu <cheramihiretucm@gmail.com>
2024-08-09 03:19:15 -0400

    More refactor

--- on-boarding/product-8/ecommerce_app/lib/features/product/data/data_resources/remote_product_data_source.dart
+++ on-boarding/product-8/ecommerce_app/lib/features/product/data/data_resources/remote_product_data_source.dart
@@ -23,48 +23,37 @@ class RemoteProductDataSourceImp implements RemoteProductDataSource {
 
   RemoteProductDataSourceImp(this.client);
 
+
+  /// Send request to server to delete a given product by the provided [id]
+  ///
+  /// Throws [ServerException] error if the request failed
   @override
   Future<int> deleteProduct(String id) =>
       executeQuery(AppData.delete, '${AppData.allProductUrl}/$id');
 
 
-
+  /// Sends request to server to get list of products from the server
+  ///
+  /// Throws [ServerException] if the request failed
   @override
-  Future<List<ProductModel>> getAllProducts() async {
-    List<ProductModel> finalResult = <ProductModel>[];
-    try {
-      final result = await client.get(Uri.parse(AppData.allProductUrl),
-          headers: AppData.jsonHeader);
-      if (result.statusCode == 200) {
-        Map<String, dynamic> jsonModel = json.decode(result.body);
-        for (Map<String, dynamic> jsonModel in jsonModel['data']) {
-          finalResult.add(ProductModel.fromJson(jsonModel));
-        }
-      } else {
-        throw ServerException();
-      }
-    } on SocketException {
-      throw ServerException();
-    }
-    return finalResult;
-  }
+  Future<List<ProductModel>> getAllProducts() => getProductRefactor(AppData.allProductUrl);
 
+  /// Sends Request to server to get single product by provided [id]
+  ///
+  /// Throws [ServerException] error if the request failed
   @override
   Future<ProductModel> getProduct(String id) async {
-    try {
-      final result = await client.get(Uri.parse('${AppData.baseUrl}/$id'),
-          headers: AppData.jsonHeader);
-      if (result.statusCode == 200) {
-        Map<String, dynamic> finalResult = json.decode(result.body);
-        return ProductModel.fromJson(finalResult['data']);
-      } else {
-        throw ServerException();
-      }
-    } on SocketException {
-      throw ServerException();
-    }
+    List<ProductModel> result = await  getProductRefactor(AppData.allProductUrl, id);
+    return result[0];
   }
 
+
+
+
+  /// Sends request to server to insert a given [productModel]
+  ///
+  ///
+  /// Throws [ServerException] if the request failed
   @override
   Future<int> insertProduct(ProductModel productModel) =>
       executeQuery(AppData.post, AppData.allProductUrl, {
@@ -74,7 +63,10 @@ class RemoteProductDataSourceImp implements RemoteProductDataSource {
         'price': '${productModel.price}',
       },);
 
-
+  /// Sends request to the server to be updated [id] in the url and [productModel] as json
+  ///
+  ///
+  /// Throws [ServerException] if request is failed
   @override
   Future<int> updateProduct(ProductModel productModel) =>
       executeQuery(AppData.put, '${AppData.allProductUrl}/${productModel.id}', {
@@ -83,6 +75,9 @@ class RemoteProductDataSourceImp implements RemoteProductDataSource {
         'price': '${productModel.price}',
       },);
 
+  /// Methods used for refactoring that is employed by inser, update, delete methods
+  ///
+  /// Throws an exceptions[ServerException] which is later also thrown by the calling methods
   Future<int> executeQuery(String requestType, String url,
       [Map<String, String>? data]) async {
     Map<String, dynamic> typeMap = {
@@ -93,7 +88,8 @@ class RemoteProductDataSourceImp implements RemoteProductDataSource {
     };
 
     try {
-      late final result;
+
+      late http.Response result;
       if (data != null) {
         result = await typeMap[requestType](
             Uri.parse(url),
@@ -113,5 +109,40 @@ class RemoteProductDataSourceImp implements RemoteProductDataSource {
       throw ServerException();
     }
   }
+
+
+  /// Refactor done for data retrival based in the existance of [id]
+  ///
+  /// Throws [ServerException] if request is failed
+  Future<List<ProductModel>> getProductRefactor(String url, [String? id]) async {
+      try {
+        late http.Response result;
+        if (id != null){
+          result = await client.get(Uri.parse('${AppData.baseUrl}/$id'),
+              headers: AppData.jsonHeader);
+        }else {
+           result = await client.get(Uri.parse(url),
+              headers: AppData.jsonHeader);
+        }
+        if (result.statusCode == 200) {
+          if (id != null){
+            Map<String, dynamic> finalResult = json.decode(result.body);
+            return [ProductModel.fromJson(finalResult['data'])];
+          }else{
+            List<ProductModel> finalResult = <ProductModel>[];
+            Map<String, dynamic> jsonModel = json.decode(result.body);
+            for (Map<String, dynamic> jsonModel in jsonModel['data']) {
+              finalResult.add(ProductModel.fromJson(jsonModel));
+            }
+            return finalResult;
+          }
+
+        } else {
+          throw ServerException();
+        }
+      } on SocketException {
+        throw ServerException();
+      }
+    }
 
 }
